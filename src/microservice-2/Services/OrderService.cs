8700e64d2fcc0bb63cc012661ca9757bdbd866e5
polatengin <polatengin@outlook.com>
2020-07-15 16:14:16 +0300

    makeorder grpc requests handled in second microservice

--- src/microservice-2/Services/OrderService.cs
+++ src/microservice-2/Services/OrderService.cs
@@ -15,4 +15,20 @@ public class OrderService : OrderServiceBase
     _logger = logger;
   }
 
+  public override Task<MakeOrderResponse> MakeOrder(MakeOrderRequest request, ServerCallContext context)
+  {
+    var headers = context.RequestHeaders;
+
+    var microserviceName = headers.FirstOrDefault(e => e.Key == "microservicename").Value;
+    var token = headers.FirstOrDefault(e => e.Key == "token").Value;
+
+    var payload = new {
+      microserviceName = microserviceName,
+      token = token
+    };
+
+    Console.WriteLine($"Received... {microserviceName} is making an order with {token} token");
+
+    return Task.FromResult(new MakeOrderResponse { Success = true });
+  }
 }
