d4607f76d02f41e12a0daba34fdba1e016588253
polatengin <polatengin@outlook.com>
2020-07-15 16:00:20 +0300

    base startup handler of the microservice 1

---
+++ src/microservice-1/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Order;
+
+namespace microservice_1
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+    }
+  }
+}
